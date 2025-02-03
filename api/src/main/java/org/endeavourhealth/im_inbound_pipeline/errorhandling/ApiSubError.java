fcc4ce7e98d405ba88d1f6397f9b3f3c8c7276cb
matt-voror <matt@voror.co.uk>
2025-02-03 09:57:59 +0000

    added cognito security and error handling layers. Added controller layout.

---
+++ api/src/main/java/org/endeavourhealth/im_inbound_pipeline/errorhandling/ApiSubError.java
@@ -0,0 +1,4 @@
+package org.endeavourhealth.im_inbound_pipeline.errorhandling;
+
+interface ApiSubError {
+}
---
+++ api/src/main/java/org/endeavourhealth/im_inbound_pipeline/errorhandling/RestAuthenticationEntryPoint.java
@@ -0,0 +1,24 @@
+package org.endeavourhealth.im_inbound_pipeline.errorhandling;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
+
+@Component
+public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
+  @Override
+  public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException ex) throws IOException, ServletException {
+      httpServletResponse.setStatus(HttpServletResponse.SC_FORBIDDEN);
+      try (OutputStream out = httpServletResponse.getOutputStream()) {
+        out.write("Access forbidden".getBytes(StandardCharsets.UTF_8));
+      }
+  }
+}
