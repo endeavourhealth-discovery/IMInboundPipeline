ee4dfd3b69151119f680ff3b93a112f9445547e0
Kostas Kardatos <kon.kardatos@gmail.com>
2025-02-24 09:48:07 +0000

    refactor db package

--- DataReader/src/main/java/org/endeavourhealth/pipeline/inbound/helpers/DBConnectionManager.java
+++ DataReader/src/main/java/org/endeavourhealth/pipeline/inbound/helpers/DBConnectionManager.java
@@ -1,20 +1,19 @@
-package org.endeavourhealth.pipeline.inbound.database.helpers;
+package org.endeavourhealth.pipeline.inbound.helpers;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Optional;
 import java.util.Properties;
 
-public class ConnectionManager {
+public class DBConnectionManager {
 
   private static final String POSTGRES_URL = Optional.ofNullable(System.getenv("POSTGRES_URL")).orElseThrow(() -> new IllegalArgumentException("Env var 'POSTGRES_URL' is not defined"));
   private static final String POSTGRES_USER = Optional.ofNullable(System.getenv("POSTGRES_USER")).orElseThrow(() -> new IllegalArgumentException("Env var 'POSTGRES_USER' is not defined"));
   private static final String POSTGRES_PASSWORD = Optional.ofNullable(System.getenv("POSTGRES_PASSWORD")).orElseThrow(() -> new IllegalArgumentException("Env var 'POSTGRES_PASSWORD' is not defined"));
   private static Connection connection = null;
 
-  private ConnectionManager() {
+  private DBConnectionManager() {
     throw new IllegalStateException("Utility class");
   }
 
