8795ac577098c38ef270be9557d5f4aa249a765e
Jo Stiansen <Jo.Stiansen@voror.co.uk>
2025-02-27 15:05:09 +0000

    added sleep to postgres script

--- artifacts/docker/setup_postgresql_container.sh
+++ artifacts/docker/setup_postgresql_container.sh
@@ -1,9 +1,9 @@
 docker run -d --name postgresDB \
     -e POSTGRES_USER=postgres \
-    -e POSTGRES_PASSWORD=123456 \
+    -e POSTGRES_PASSWORD=password \
     -e POSTGRES_DB=discovery \
     -p 5432:5432 \
     postgres
-
+sleep 5
 docker cp ../database/HealthDB-Postgres.sql postgresDB:/database.sql
 docker exec -i postgresDB psql -U postgres -f database.sql
