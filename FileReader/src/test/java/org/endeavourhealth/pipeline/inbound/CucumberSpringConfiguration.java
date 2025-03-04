824b06288b30d7648ce5d32778b77a265936dfd8
Kostas Kardatos <kon.kardatos@gmail.com>
2025-03-04 16:03:30 +0000

    cucumber setup WIP

---
+++ FileReader/src/test/java/org/endeavourhealth/pipeline/inbound/CucumberSpringConfiguration.java
@@ -0,0 +1,10 @@
+package org.endeavourhealth.pipeline.inbound;
+
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@CucumberContextConfiguration
+@SpringBootTest
+public class CucumberSpringConfiguration {
+
+}
---
+++ FileReader/src/test/java/org/endeavourhealth/pipeline/inbound/validator/FileValidatorSteps.java
@@ -0,0 +1,43 @@
+package org.endeavourhealth.pipeline.inbound.validator;
+
+import io.cucumber.java.en.*;
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.endeavourhealth.pipeline.inbound.CucumberRunner;
+import org.endeavourhealth.pipeline.inbound.CucumberSpringConfiguration;
+import org.junit.jupiter.api.Assertions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FileValidatorSteps extends CucumberSpringConfiguration {
+  private String fileName;
+  private List<String> fileHeaders;
+  private boolean validationResult;
+
+  @Autowired
+  private FileValidator validator; // Inject Spring Bean
+
+  @Given("a file named {string}")
+  public void a_file_named(String fileName) {
+    this.fileName = fileName;
+  }
+
+  @And("it contains headers {string}")
+  public void it_contains_headers(String headers) {
+    this.fileHeaders = Arrays.asList(headers.split(", "));
+  }
+
+  @When("I validate the file")
+  public void i_validate_the_file() {
+//    this.validationResult = validator.isValidFile(fileName, fileHeaders);
+    this.validationResult = true;
+  }
+
+  @Then("the validation should return {word}")
+  public void the_validation_should_return(String expectedResult) {
+    boolean expected = Boolean.parseBoolean(expectedResult);
+    Assertions.assertEquals(expected, validationResult);
+  }
+}
