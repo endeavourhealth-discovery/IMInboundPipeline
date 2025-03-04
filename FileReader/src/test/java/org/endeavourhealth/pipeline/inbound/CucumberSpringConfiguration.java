ec1976d292fc06844b2c53da4bcc2d9b0b47cdf5
Richard Collier <rich@voror.co.uk>
2025-03-04 17:23:02 +0000

    Update RabbitMQ properties configuration in tests
    Moved RabbitMQ properties from inline annotations to external `application.properties` file for better manageability and clarity. This improves the separation of configuration from code and simplifies future modifications.

--- FileReader/src/test/java/org/endeavourhealth/pipeline/inbound/CucumberSpringConfiguration.java
+++ FileReader/src/test/java/org/endeavourhealth/pipeline/inbound/CucumberSpringConfiguration.java
@@ -4,12 +4,7 @@ import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @CucumberContextConfiguration
-@SpringBootTest(
-  properties = {
-    "rabbitmq.targetBaseRoutingKey=EMIS",
-    "rabbitmq.sourceQueue=File-EMIS",
-    "rabbitmq.targetExchange=Data"
-  })
+@SpringBootTest
 public class CucumberSpringConfiguration {
 
 }
