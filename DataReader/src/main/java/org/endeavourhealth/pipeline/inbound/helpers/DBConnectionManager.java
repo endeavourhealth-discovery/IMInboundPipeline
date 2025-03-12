54c10168ccf809128bea11e7091f7c80b8b8f5ba
Jo Stiansen <Jo.Stiansen@voror.co.uk>
2025-03-12 13:46:09 +0000

    refactored db filing

--- DataReader/src/main/java/org/endeavourhealth/pipeline/inbound/helpers/DBConnectionManager.java
+++ DataReader/src/main/java/org/endeavourhealth/pipeline/inbound/helpers/DBConnectionManager.java
@@ -15,7 +15,20 @@ public class DBConnectionManager {
   //private static final String POSTGRES_USER = Optional.ofNullable(System.getenv("SPRING_DATASOURCE_USER")).orElseThrow(() -> new IllegalArgumentException("Env var 'POSTGRES_USER' is not defined"));
   //private static final String POSTGRES_PASSWORD = Optional.ofNullable(System.getenv("SPRING_DATASOURCE_PASSWORD")).orElseThrow(() -> new IllegalArgumentException("Env var 'POSTGRES_PASSWORD' is not defined"));
 
+
   private static final Logger LOG = LoggerFactory.getLogger(DBConnectionManager.class);
+  private static PreparedStatement upsertEvent = null;
+  private static PreparedStatement upsertInstance = null;
+
+  static {
+    try {
+      upsertEvent = prepareUpsertEvent();
+      upsertInstance = prepareUpsertInstance();
+    } catch (SQLException e) {
+      LOG.error("{}", e.toString());
+      System.exit(1);
+    }
+  }
 
   private DBConnectionManager() {
     throw new IllegalStateException("Utility class");
@@ -28,23 +41,27 @@ public class DBConnectionManager {
     return DriverManager.getConnection(DBConfig.getSpringDatasourceUrl(), props);
   }
 
-  public static void fileEvent(Connection connection, Event event) throws SQLException {
-    try (PreparedStatement statement = connection.prepareStatement("INSERT INTO healthdb.event (id, json) VALUES (?,(?::json)) ON CONFLICT (id) DO UPDATE SET json=?::json")) {
-      statement.setObject(1, event.getId());
-      statement.setString(2, event.getJson());
-      statement.setString(3, event.getJson());
-      statement.executeUpdate();
-      LOG.debug("Event filed to database");
-    }
+  private static PreparedStatement prepareUpsertEvent() throws SQLException {
+    return getConnection().prepareStatement("INSERT INTO healthdb.event (id, json) VALUES (?,(?::json)) ON CONFLICT (id) DO UPDATE SET json=?::json");
   }
 
-  public static void fileInstance(Connection connection, Instance instance) throws SQLException {
-    try (PreparedStatement statement = connection.prepareStatement("INSERT INTO healthdb.instance (id, json) VALUES (?,(?::json)) ON CONFLICT (id) DO UPDATE SET json=?::json")) {
-      statement.setObject(1, instance.getId());
-      statement.setString(2, instance.getJson());
-      statement.setString(3, instance.getJson());
-      statement.executeUpdate();
-      LOG.debug("Instance filed database");
-    }
+  private static PreparedStatement prepareUpsertInstance() throws SQLException {
+    return getConnection().prepareStatement("INSERT INTO healthdb.instance (id, json) VALUES (?,(?::json)) ON CONFLICT (id) DO UPDATE SET json=?::json");
+  }
+
+  public static void fileEvent(Event event) throws SQLException {
+    upsertEvent.setObject(1, event.getId());
+    upsertEvent.setString(2, event.getJson());
+    upsertEvent.setString(3, event.getJson());
+    upsertEvent.executeUpdate();
+    LOG.debug("Event filed to database");
+  }
+
+  public static void fileInstance(Instance instance) throws SQLException {
+    upsertInstance.setObject(1, instance.getId());
+    upsertInstance.setString(2, instance.getJson());
+    upsertInstance.setString(3, instance.getJson());
+    upsertInstance.executeUpdate();
+    LOG.debug("Instance filed database");
   }
 }
