619e1af3e30bdb6099affc1c5b0690e9319f898c
Richard Collier <rich@voror.co.uk>
2025-03-21 15:34:18 +0000

    Add Cucumber testing framework and enhance Transformer logic
    Replaced JUnit-based test with Cucumber for better test coverage and added `formatDateTime` in Transformer to handle dates with or without time. Enhanced test configurations and build dependencies to support Cucumber integration and modified DBConnectionManager for improved parameter handling.

---
+++ Transform/src/test/java/org/endeavourhealth/pipeline/inbound/CucumberSpringConfiguration.java
@@ -0,0 +1,10 @@
+package org.endeavourhealth.pipeline.inbound;
+
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@CucumberContextConfiguration
+@SpringBootTest(classes = TestConfig.class)
+public class CucumberSpringConfiguration {
+
+}
---
+++ Transform/src/test/java/org/endeavourhealth/pipeline/inbound/TestConfig.java
@@ -0,0 +1,9 @@
+package org.endeavourhealth.pipeline.inbound;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"org.endeavourhealth.pipeline.inbound"}) // Adjust package as necessary
+public class TestConfig {
+}
